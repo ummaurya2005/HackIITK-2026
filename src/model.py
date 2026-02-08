0f2c82769a3592893ba93dcb34514e4a359e2e3c
Uttam Maurya <ummauryas2005@gmail.com>
2026-02-08 23:44:02 +0530

    correct the errors
    Uncomment the AudioClassifier class definition and its methods.

--- src/model.py
+++ src/model.py
@@ -1,23 +1,3 @@
-# import torch.nn as nn
-
-# class AudioClassifier(nn.Module):
-#     def __init__(self, input_dim=120, num_classes=2):
-#         super().__init__()
-#         self.net = nn.Sequential(
-#             nn.Linear(input_dim, 256),
-#             nn.BatchNorm1d(256),
-#             nn.ReLU(),
-#             nn.Dropout(0.4),
-
-#             nn.Linear(256, 128),
-#             nn.ReLU(),
-#             nn.Dropout(0.3),
-
-#             nn.Linear(128, num_classes)
-#         )
-
-#     def forward(self, x):
-#         return self.net(x)
 import torch.nn as nn
 
 class AudioClassifier(nn.Module):
